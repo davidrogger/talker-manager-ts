a9c1980c664bc970ea8ae8bbfa0031a1363e1d9f
davidrogger <davidrogger.a@gmail.com>
2023-07-03 14:05:23 -0300

    FEAT: update email as unique

--- migration.sql
+++ migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE user (
   id VARCHAR(10) PRIMARY KEY,
   first_name VARCHAR(15) NOT NULL,
   last_name VARCHAR(15) NOT NULL,
-  email VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
   password VARCHAR(50) NOT NULL
 );
 
