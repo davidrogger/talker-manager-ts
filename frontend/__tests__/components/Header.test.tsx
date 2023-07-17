bc56f5e6b4e2df51b4d4385fd28d2dbefb127df0
davidrogger <davidrogger.a@gmail.com>
2023-07-17 15:24:29 -0300

    TEST: header title

---
+++ frontend/__tests__/components/Header.test.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/Header';
+import { render, screen } from '@testing-library/react';
+
+describe('Testing Component <Header />', () => {
+  it('Should render the title "Talker Manager"', () => {
+    render(<Header />)
+    const title = screen.getByRole('heading', { name: /talker manager/i });
+    expect(title).toBeInTheDocument();
+  });
+});
