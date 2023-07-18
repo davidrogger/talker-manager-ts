9f8aba9168863c952b6d23863e6b8d3640563c12
davidrogger <davidrogger.a@gmail.com>
2023-07-18 10:37:56 -0300

    TEST: Header elements

--- frontend/__tests__/components/Header.test.tsx
+++ frontend/__tests__/components/Header.test.tsx
@@ -2,9 +2,11 @@ import Header from '@/components/Header';
 import { render, screen } from '@testing-library/react';
 
 describe('Testing Component <Header />', () => {
-  it('Should render the title "Talker Manager"', () => {
+  it('Should render header elements', () => {
     render(<Header />);
-    const title = screen.getByRole('heading', { name: /talker manager/i });
+    const title = screen.getByText('Talker Manager');
+    const login = screen.getByText('Login');
     expect(title).toBeInTheDocument();
+    expect(login).toBeInTheDocument();
   });
 });
