06a11f6ea9fe0d5b9859f2cbd365c3893dd23627
davidrogger <davidrogger.a@gmail.com>
2023-07-18 10:38:07 -0300

    FEAT: Header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,25 @@
+import Link from 'next/link';
+
 function Header() {
   return (
-    <div>
-      <h1>Talker Manager</h1>
+    <div
+      data-testid='header-container-id'
+      className="flex items-center h-14 bg-slate-500 p-2 justify-between text-white mb-3 shadow-lg"
+    >
+      <h1
+        className='text-2xl'
+      >
+        Talker Manager
+      </h1>
+
+      <Link
+        className='relative inline-block hover:after'
+        href="/login"
+      >
+        <span className='hover-underline-animation'>
+          Login
+        </span>
+      </Link>
     </div>
   );
 }
