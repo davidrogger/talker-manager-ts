1287caea07030002a57ff440096fb4c747d0bf93
davidrogger <davidrogger.a@gmail.com>
2023-07-25 11:18:02 -0300

    FEAT: added cors to the API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import loginRoutes from './routes/login.route';
 import registerRoutes from './routes/register.route';
 import errorResponse from './middlwares/error.response';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/login', loginRoutes);
 app.use('/register', registerRoutes);
