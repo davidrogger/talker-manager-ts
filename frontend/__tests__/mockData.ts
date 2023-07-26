af2e2d0418458d998355bacc408d8eb40b8a19fe
davidrogger <davidrogger.a@gmail.com>
2023-07-26 16:07:16 -0300

    CHORE: remove mockdata

--- frontend/__tests__/mockData.ts
+++
@@ -1,9 +0,0 @@
-export function mockSuccessLogin<T>(response:T) {
-  return {
-    ok: true,
-    status: 200,
-    json: async () => Promise.resolve({ response }),
-  } as Response;
-}
-
-export const placeholder = '';
