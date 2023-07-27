bf0e88849462bf67b73584be22b7bb05d9f27e11
davidrogger <davidrogger.a@gmail.com>
2023-07-27 10:12:09 -0300

    TEST: login api authentication and route

--- frontend/__tests__/pages/Login.test.tsx
+++ frontend/__tests__/pages/Login.test.tsx
@@ -1,13 +1,13 @@
-import Login from '@/app/login/page';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import axios from 'axios';
-import { AuthProvider } from '@/contexts/Auth';
 
-import { RouterContext } from 'next/dist/shared/lib/router-context';
-import createMockRouter from '../utils/createMockRouter';
+import Login from '@/app/login/page';
+
+import { api } from '@/services/api';
+import { renderWithProviders } from '../utils/mockRouter';
 
 describe('Testing page Login', () => {
+  afterAll(() => jest.restoreAllMocks());
+
   it('Should render all elements from page Login', () => {
     render(<Login />);
     const title = screen.getByRole('heading', { name: 'Manager Login' });
@@ -25,42 +25,32 @@ describe('Testing page Login', () => {
   });
 
   it('Should render a error message when trying to login without input any value', async () => {
-    render(
-      <RouterContext.Provider value={createMockRouter({})}>
-        <AuthProvider>
-          <Login />
-        </AuthProvider>,
-      </RouterContext.Provider>,
-    );
+    const { user } = renderWithProviders(<Login />);
 
     const btnEnter = screen.getByRole('button', { name: 'Enter' });
-    await userEvent.click(btnEnter);
+    await user.click(btnEnter);
 
     const warningMsg = await screen.findByText(/please you need to fill the email and password/i);
     expect(warningMsg).toBeInTheDocument();
   });
 
   it('Should request authentication to the API and go to the dashboard', async () => {
-    const mockFetch = jest.spyOn(axios, 'post').mockResolvedValue({ data: {} });
-    render(
-      <RouterContext.Provider value={createMockRouter({})}>
-        <AuthProvider>
-          <Login />
-        </AuthProvider>,
-      </RouterContext.Provider>,
-    );
+    const mockApi = jest.spyOn(api, 'post')
+      .mockResolvedValue({ data: { token: 'valid-token' } });
+
+    const { user, router } = renderWithProviders(<Login />);
+
+    const userInput = { email: 'teste@teste.com', password: '123456' };
 
     const inputEmail = screen.getByPlaceholderText('Email');
     const inputPassword = screen.getByPlaceholderText('Password');
     const btnEnter = screen.getByRole('button', { name: 'Enter' });
 
-    await userEvent.type(inputEmail, 'teste@teste.com');
-    await userEvent.type(inputPassword, '123456');
-    await userEvent.click(btnEnter);
+    await user.type(inputEmail, userInput.email);
+    await user.type(inputPassword, userInput.password);
+    await user.click(btnEnter);
 
-    expect(mockFetch).toHaveBeenCalledTimes(1);
-
-    const dashboardTitle = await screen.findByRole('heading', { name: 'Dashboard' });
-    expect(dashboardTitle).toBeInTheDocument();
+    expect(mockApi).toHaveBeenCalledWith('/users', userInput);
+    expect(router.push).toHaveBeenCalledWith('/dashboard');
   });
 });
