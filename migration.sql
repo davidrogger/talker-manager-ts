ea11d6206118ee15fa0f2b6d4c4e5b2dcbc39896
davidrogger <davidrogger.a@gmail.com>
2023-07-27 11:18:52 -0300

    CHORE: update manager password crypt

--- migration.sql
+++ migration.sql
@@ -23,4 +23,4 @@ CREATE TABLE lecture (
 );
 
 INSERT INTO user (id, first_name, last_name, email, password)
-VALUES ('387f4c72-c314-456c-b00f-bd63a0a7ebd7', 'Talker', 'Manager', 'talker@manager.com', '123pass');
+VALUES ('387f4c72-c314-456c-b00f-bd63a0a7ebd7', 'Talker', 'Manager', 'talker@manager.com', '$2b$10$hOuksoM1FCnUIWSK1xDg/.DccLN5SaNroTjmbsMC0Tcdv0xd8xdAi'); -- 123pass
