3063ab62bca26fed605c87fdc14ede3bfeeb4687
davidrogger <davidrogger.a@gmail.com>
2023-07-27 11:47:37 -0300

    FEAT: handle axios error response

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,15 +1,26 @@
 import { LoginReponse, LoginInput } from '@/types';
 import axios from 'axios';
 
+type AxiosErrorResponse = {
+  response: {
+    data: {
+      message: string,
+    }
+  }
+}
+
 const URL = process.env.API_URL || 'http://localhost:3001';
 
 export const api = axios.create({ baseURL: URL });
 
 export async function loginAuth(user:LoginInput):Promise<LoginReponse> {
   try {
-    const { data: { token } } = await api.post('/users', user);
+    const { data: { token } } = await api.post('/login', user);
     return { token };
   } catch (error) {
-    return { error } as LoginReponse;
+    const axiosError = error as AxiosErrorResponse;
+    const { data: { message } } = axiosError.response;
+
+    return { error: { message } } as LoginReponse;
   }
 }
