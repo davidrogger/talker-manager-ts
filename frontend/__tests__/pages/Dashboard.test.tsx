71c0012238b00436a72e78772296a124bceae8ee
davidrogger <davidrogger.a@gmail.com>
2023-08-01 08:56:19 -0300

    TEST: header elements in dashboard logged

--- frontend/__tests__/pages/Dashboard.test.tsx
+++ frontend/__tests__/pages/Dashboard.test.tsx
@@ -8,4 +8,16 @@ describe.only('Testing page Dashboard', () => {
     expect(screen.getByText('Dashboard')).toBeInTheDocument();
     expect(screen.getByTestId('header-container-id')).toBeInTheDocument();
   });
+
+  it('Should have the logged elements in the header', () => {
+    RenderPage({ route: '/dashboard' });
+
+    const logout = screen.getByTestId('header-logout-nav-id');
+    const dashboard = screen.getByTestId('header-dashboard-nav-id');
+    const home = screen.getByTestId('header-home-nav-id');
+
+    expect(logout).toBeInTheDocument();
+    expect(dashboard).toBeInTheDocument();
+    expect(home).toBeInTheDocument();
+  });
 });
