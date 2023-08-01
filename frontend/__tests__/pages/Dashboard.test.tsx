6848b16f8a77ce10cc6c69ff715db0c82e080578
davidrogger <davidrogger.a@gmail.com>
2023-08-01 11:49:40 -0300

    TEST: render user data in the dashboard

--- frontend/__tests__/pages/Dashboard.test.tsx
+++ frontend/__tests__/pages/Dashboard.test.tsx
@@ -1,4 +1,6 @@
 import Dashboard from '@/app/dashboard/page';
+import { AuthProvider } from '@/contexts/Auth';
+import { api } from '@/services/api';
 import { render, screen } from '@testing-library/react';
 
 describe('Testing page Dashboard', () => {
@@ -8,5 +10,26 @@ describe('Testing page Dashboard', () => {
     const title = screen.getByRole('heading', { name: 'Dashboard' });
 
     expect(title).toBeInTheDocument();
+  });
+
+  it('Should render the name of the manager logged in the dashboard painel', async () => {
+    const mockUser = {
+      firstName: 'Jonas',
+      lastName: 'Doe',
+      email: 'jonasdoe@testes.com',
+    };
+
+    const mockLocalStorage = jest.spyOn(localStorage, 'get').mockReturnValue('');
+    const mockAPI = jest.spyOn(api, 'getUserData').mockResolvedValue(mockUser);
+
+    render(
+      <AuthProvider>
+        <Dashboard />
+      </AuthProvider>,
+    );
+
+    expect(mockLocalStorage).toHaveBeenCalled();
+    expect(mockAPI).toHaveBeenCalled();
+    expect(await screen.findByText('Jonas Doe')).toBeInTheDocument();
   });
 });
