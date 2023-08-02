437b9f62ab795bd49671942167a39dd84a6b889e
davidrogger <davidrogger.a@gmail.com>
2023-08-02 14:14:15 -0300

    CHORE: rename mock provider next file name

--- frontend/__tests__/utils/RenderWithAuthProvider.tsx
+++ frontend/__tests__/utils/RenderWithAuthProvider.tsx
@@ -1,12 +1,17 @@
 import { AuthProvider } from '@/contexts/Auth';
 
 import React from 'react';
-import RenderMockContextProviderNavigation from './RenderMockNavigationProvider';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
+import RenderMockContextProviderNavigation from './RenderMockContextProviderNavigation';
 
-export default function RenderWithAuthProvider(children:React.JSX.Element) {
+export default function RenderWithAuthProvider(
+  children:React.JSX.Element,
+  router:Partial<AppRouterInstance> = {},
+) {
   return RenderMockContextProviderNavigation(
     <AuthProvider>
       { children }
     </AuthProvider>,
+    router,
   );
 }
