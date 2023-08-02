3f3cccb4065d302f4a539ff6ad92556409c7ff95
davidrogger <davidrogger.a@gmail.com>
2023-08-02 14:13:15 -0300

    TEST: redirect from dashboard

--- frontend/__tests__/pages/Dashboard.test.tsx
+++ frontend/__tests__/pages/Dashboard.test.tsx
@@ -4,6 +4,8 @@ import { screen } from '@testing-library/react';
 import RenderWithAuthProvider from '../utils/RenderWithAuthProvider';
 
 describe('Testing page Dashboard', () => {
+  beforeEach(() => jest.restoreAllMocks());
+
   it('Should render the name of the manager logged in the dashboard painel', async () => {
     const mockUser = {
       data: {
@@ -28,9 +30,24 @@ describe('Testing page Dashboard', () => {
   });
 
   it('Should redirect to home page when missing a token in the localStorage', async () => {
-    jest.spyOn(Object.getPrototypeOf(window.localStorage), 'getItem').mockReturnValue('');
+    const mockLocalStorage = jest.spyOn(Object.getPrototypeOf(window.localStorage), 'getItem').mockReturnValue('');
 
     const { mockRouter } = RenderWithAuthProvider(<Dashboard />);
+
+    expect(mockLocalStorage).toHaveBeenCalledWith('talker-token');
+    await Promise.resolve();
+    expect(mockRouter.push).toHaveBeenCalledWith('/');
+  });
+
+  it('Should redirect to home page when the token is invalid', async () => {
+    const mockResponse = { response: { data: { message: 'Invalid Token' } } };
+    jest.spyOn(Object.getPrototypeOf(window.localStorage), 'getItem').mockReturnValue('invalid-token');
+    jest.spyOn(Object.getPrototypeOf(window.localStorage), 'clear');
+    const mockAPI = jest.spyOn(api, 'get').mockRejectedValue(mockResponse);
+
+    const { mockRouter } = await RenderWithAuthProvider(<Dashboard />);
+
+    await expect(mockAPI).toHaveBeenCalled();
     expect(mockRouter.push).toHaveBeenCalledWith('/');
   });
 });
