574324e9b77e9268f4bfec84a079b755be748443
davidrogger <davidrogger.a@gmail.com>
2023-08-03 09:37:55 -0300

    TEST: check email format when creating a new account

--- backend/tests/integration/register.test.ts
+++ backend/tests/integration/register.test.ts
@@ -7,7 +7,7 @@ import jwt from 'jsonwebtoken';
 import { ResultSetHeader } from 'mysql2';
 import app from '../../src/app';
 import connection from '../../src/models/connection.model';
-import { IUser } from '../types';
+import { IUser } from '../../src/types';
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -106,6 +106,43 @@ describe('Route /register', () => {
         .send(user);
       expect(status).to.be.equal(409);
       expect(body.message).to.be.equal('Email address already in use');
+    });
+  });
+
+  describe('Email and password need to follow a standard format', () => {
+    it('Should return status 400 when the email is in a bad format', async () => {
+      sinon.stub(connection, 'execute').resolves([[], []]);
+
+      const badEmails = [
+        {
+          firstName: 'Jonas1',
+          lastName: 'Doe1',
+          email: 'invalidemail.com',
+          password: '123456',
+        },
+        {
+          firstName: 'Jonas2',
+          lastName: 'Doe2',
+          email: 'invalidemailcom',
+          password: '123456',
+        },
+        {
+          firstName: 'Jonas3',
+          lastName: 'Doe3',
+          email: 'invalid@emailcom',
+          password: '123456',
+        },
+      ];
+
+      await Promise.all(badEmails.map(async (newAccount) => {
+        const { status, body } = await chai
+          .request(app)
+          .post(registerEndpoint)
+          .send(newAccount);
+
+        expect(status).to.be.equal(400);
+        expect(body.message).to.be.equal('Invalid Email format');
+      }));
     });
   });
 });
