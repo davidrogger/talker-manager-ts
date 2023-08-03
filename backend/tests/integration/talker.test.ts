71c7b82a140f7f34c1fff5906e85592227e78b46
davidrogger <davidrogger.a@gmail.com>
2023-08-03 11:48:48 -0300

    TEST: required token when trying to get data from all talkers

--- backend/tests/integration/talker.test.ts
+++ backend/tests/integration/talker.test.ts
@@ -11,15 +11,43 @@ import { mockTalkers } from './_mockData';
 chai.use(chaiHttp);
 const { expect } = chai;
 
+const talkerEndpoint = '/talker';
+
 describe('Testing route /talker', () => {
+  beforeEach(sinon.restore);
+
   it('Should return all talker register in the data base', async () => {
     sinon.stub(connection, 'execute').resolves([mockTalkers, []]);
 
     const { status, body } = await chai
       .request(app)
-      .get('/talker');
+      .get(talkerEndpoint);
 
     expect(status).to.be.equal(200);
     expect(body.talkers).to.be.deep.equal(mockTalkers);
+  });
+
+  it('Should return return 401 when missing or is a invalid token', async () => {
+    sinon.stub(connection, 'execute').resolves([[], []]); // to avoid ping the mysql in the first tests
+    const tokensTest = [
+      {
+        token: '',
+        expectMessage: 'Missing Token',
+      },
+      {
+        token: 'invalid-token',
+        expectMessage: 'Invalid Token',
+      },
+    ];
+
+    await Promise.all(tokensTest.map(async ({ token, expectMessage }) => {
+      const { status, body } = await chai
+        .request(app)
+        .get(talkerEndpoint)
+        .set('Authorization', token);
+
+      expect(status).to.be.equal(401);
+      expect(body.message).to.be.equal(expectMessage);
+    }));
   });
 });
