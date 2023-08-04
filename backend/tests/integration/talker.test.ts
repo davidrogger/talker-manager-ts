2ebdf00736265e94f534725646843d10e19f98dc
davidrogger <davidrogger.a@gmail.com>
2023-08-04 08:51:57 -0300

    TEST: divided describe talker between GET and POST requests

--- backend/tests/integration/talker.test.ts
+++ backend/tests/integration/talker.test.ts
@@ -18,42 +18,46 @@ const talkerEndpoint = '/talker';
 describe('Testing route /talker', () => {
   beforeEach(sinon.restore);
 
-  it('Should return all talker register in the data base', async () => {
-    sinon.stub(connection, 'execute').resolves([mockTalkers, []]);
-    sinon.stub(jwt, 'verify').callsFake(() => mockPublicUserData);
+  describe('Get request', () => {
+    it('Should return all talker register in the data base', async () => {
+      sinon.stub(connection, 'execute').resolves([mockTalkers, []]);
+      sinon.stub(jwt, 'verify').callsFake(() => mockPublicUserData);
 
-    const { status, body } = await chai
-      .request(app)
-      .get(talkerEndpoint)
-      .set('authorization', 'valid-token');
-
-    expect(status).to.be.equal(200);
-    expect(body.talkers).to.be.deep.equal(mockTalkers);
-  });
-
-  it('Should return return 401 when missing or is a invalid token', async () => {
-    sinon.stub(connection, 'execute').resolves([[], []]); // to avoid ping the mysql in the first tests
-    sinon.stub(jwt, 'verify').throws();
-
-    const tokensTest = [
-      {
-        token: '',
-        expectMessage: 'Missing Token',
-      },
-      {
-        token: 'invalid-token',
-        expectMessage: 'Invalid Token',
-      },
-    ];
-
-    await Promise.all(tokensTest.map(async ({ token, expectMessage }) => {
       const { status, body } = await chai
         .request(app)
         .get(talkerEndpoint)
-        .set('Authorization', token);
+        .set('authorization', 'valid-token');
 
-      expect(status).to.be.equal(401);
-      expect(body.message).to.be.equal(expectMessage);
-    }));
+      expect(status).to.be.equal(200);
+      expect(body.talkers).to.be.deep.equal(mockTalkers);
+    });
+
+    it('Should return return 401 when missing or is a invalid token', async () => {
+      sinon.stub(connection, 'execute').resolves([[], []]); // to avoid ping the mysql in the first tests
+      sinon.stub(jwt, 'verify').throws();
+
+      const tokensTest = [
+        {
+          token: '',
+          expectMessage: 'Missing Token',
+        },
+        {
+          token: 'invalid-token',
+          expectMessage: 'Invalid Token',
+        },
+      ];
+
+      await Promise.all(tokensTest.map(async ({ token, expectMessage }) => {
+        const { status, body } = await chai
+          .request(app)
+          .get(talkerEndpoint)
+          .set('Authorization', token);
+
+        expect(status).to.be.equal(401);
+        expect(body.message).to.be.equal(expectMessage);
+      }));
+    });
   });
+
+  describe('POST request', () => {});
 });
