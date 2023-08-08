c874e238c90b1046c1336f5fb05103825cd078f7
davidrogger <davidrogger.a@gmail.com>
2023-08-08 15:39:11 -0300

    REFACT: controller me route division

--- backend/src/routes/me.route.ts
+++ backend/src/routes/me.route.ts
@@ -1,22 +1,15 @@
 import { Router } from 'express';
 
 import * as validate from '@middlewares/request.validation';
-import * as jwtService from '@services/jwt.service';
+
+import * as meController from '@controllers/me.controller';
 
 const route = Router();
 
 route.get(
   '/',
   validate.tokenRequired,
-  async (req, res, next) => {
-    const { authorization } = req.headers;
-    try {
-      const user = jwtService.verifyToken(authorization as string);
-      res.status(200).json({ user });
-    } catch (error) {
-      next(error);
-    }
-  },
+  meController.getUserData,
 );
 
 export default route;
