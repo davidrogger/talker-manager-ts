a8ec27afab5f9cc0bdc0f349e327878f8177a94c
davidrogger <davidrogger.a@gmail.com>
2023-08-08 15:28:50 -0300

    REFACT: controller lecture route division

---
+++ backend/src/controllers/lecture.controller.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from 'express';
+
+import * as idService from '@services/id.service';
+import * as lectureService from '@services/lecture.service';
+
+export async function getAllLectures(req:Request, res:Response, next:NextFunction) {
+  try {
+    const lectures = await lectureService.getAllLectures();
+    res.status(200).json({ lectures });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function createLecture(req:Request, res:Response, next:NextFunction) {
+  const newLecture = req.body;
+  try {
+    const id = idService.generateId();
+    const lecture = { id, ...newLecture };
+    const message = await lectureService.createLecture(lecture);
+
+    res.status(201).json({ message });
+  } catch (error) {
+    next(error);
+  }
+}
--- backend/src/routes/lecture.route.ts
+++ backend/src/routes/lecture.route.ts
@@ -1,19 +1,12 @@
 import { Router } from 'express';
 
-import * as lectureService from '@services/lecture.service';
-import * as idService from '@services/id.service';
 import * as validation from '@middlewares/request.validation';
+
+import * as lectureController from '@controllers/lecture.controller';
 
 const route = Router();
 
-route.get('/', async (_req, res, next) => {
-  try {
-    const lectures = await lectureService.getAllLectures();
-    res.status(200).json({ lectures });
-  } catch (error) {
-    next(error);
-  }
-});
+route.get('/', lectureController.getAllLectures);
 
 route.post(
   '/',
@@ -22,18 +15,7 @@ route.post(
   validation.lectureTalkerNameField,
   validation.lectureTitleField,
   validation.lectureWatchedAtField,
-  async (req, res, next) => {
-    const newLecture = req.body;
-    try {
-      const id = idService.generateId();
-      const lecture = { id, ...newLecture };
-      const message = await lectureService.createLecture(lecture);
-
-      res.status(201).json({ message });
-    } catch (error) {
-      next(error);
-    }
-  },
+  lectureController.createLecture,
 );
 
 export default route;
