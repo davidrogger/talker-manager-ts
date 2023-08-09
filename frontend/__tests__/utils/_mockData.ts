28313a5973bf8851599888dbc42717d746395d6f
davidrogger <davidrogger.a@gmail.com>
2023-08-09 09:42:05 -0300

    CHORE: mockLecture response data

--- frontend/__tests__/utils/_mockData.ts
+++ frontend/__tests__/utils/_mockData.ts
@@ -8,4 +8,53 @@ export const mockUserDataResponse = {
   },
 };
 
-export const placeholder = '';
+export const mockLectures = [
+  {
+    id: '1b9babeb-f5b0-4d4c-9bd9-27918d2430d4',
+    talkerName: 'Davíd Roggér',
+    title: 'Practicing Frontend',
+    watchedAt: '08/08/2023',
+  },
+  {
+    id: '1dad1076-5b1c-4194-b480-18b8808b471b',
+    talkerName: 'Davíd Roggér',
+    title: 'Practicing Backend',
+    watchedAt: '08/08/2023',
+  },
+  {
+    id: '262cd336-50ea-4a9e-83b5-a3c829e140f5',
+    talkerName: 'Davíd Roggér',
+    title: 'Practicing FullStack',
+    watchedAt: '08/08/2023',
+  },
+  {
+    id: '7db8a73f-e461-4bb7-bc04-fe1deae5f3ca',
+    talkerName: 'Davíd Roggér',
+    title: 'Practicing Jest',
+    watchedAt: '08/08/2023',
+  },
+  {
+    id: '807165e3-3ead-4263-be3e-b5c0f066aad6',
+    talkerName: 'Davíd Roggér',
+    title: 'Practicing Express',
+    watchedAt: '08/08/2023',
+  },
+  {
+    id: '89534693-52d2-4d68-936b-fb64e758c866',
+    talkerName: 'Davíd Roggér',
+    title: 'Practicing Typescript',
+    watchedAt: '08/08/2023',
+  },
+  {
+    id: '8fb64a73-971c-44f9-9e84-08c9fa258978',
+    talkerName: 'Davíd Roggér',
+    title: 'Practicing Nextjs',
+    watchedAt: '08/08/2023',
+  },
+  {
+    id: 'be2d8790-d7be-464f-992e-26dc29025d85',
+    talkerName: 'Davíd Roggér',
+    title: 'Practicing Docker',
+    watchedAt: '08/08/2023',
+  },
+];
