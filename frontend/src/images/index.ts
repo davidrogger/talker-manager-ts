de7cefd53989b3002355cea077c40e73a5055ce7
davidrogger <davidrogger.a@gmail.com>
2023-08-11 09:30:14 -0300

    CHORE: added new button images

---
+++ frontend/src/images/index.ts
@@ -0,0 +1,14 @@
+import cancel from './cancel.svg';
+import confirm from './confirm.svg';
+import edit from './edit.svg';
+
+import closeEye from './closeEye.svg';
+import openEye from './openEye.svg';
+
+export default {
+  cancel,
+  confirm,
+  edit,
+  closeEye,
+  openEye,
+};
