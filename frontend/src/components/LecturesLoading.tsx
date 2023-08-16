935aca65449a639c99e94341e9eba74cd22b63e4
davidrogger <davidrogger.a@gmail.com>
2023-08-16 09:58:59 -0300

    STYLE<lectures skeleton>: removed unnecessary old divs

--- frontend/src/components/LecturesLoading.tsx
+++ frontend/src/components/LecturesLoading.tsx
@@ -4,11 +4,5 @@ import { createEmptyList } from '@/utils';
 export default function LecturesLoading() {
   const squeleton = createEmptyList(12);
 
-  return (
-    <div>
-      <div className='flex flex-wrap content-start justify-center pt-8 h-[calc(100vh-112px)] bg-gray-100'>
-        {squeleton.map((key) => <LectureCardLoading key={key} />)}
-      </div>
-    </div>
-  );
+  return squeleton.map((key) => (<LectureCardLoading key={key} />));
 }
