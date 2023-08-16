71953791ace4d87af6fce48e863db8ee8c985b14
davidrogger <davidrogger.a@gmail.com>
2023-08-16 10:04:48 -0300

    CHORE: changed way to fake empty array lists

--- frontend/src/components/LecturesLoading.tsx
+++ frontend/src/components/LecturesLoading.tsx
@@ -1,8 +1,5 @@
 import LectureCardLoading from '@/components/LectureCard-loading';
-import { createEmptyList } from '@/utils';
 
 export default function LecturesLoading() {
-  const squeleton = createEmptyList(12);
-
-  return squeleton.map((key) => (<LectureCardLoading key={key} />));
+  return Array.from({ length: 8 }, (_, key) => (<LectureCardLoading key={key} />));
 }
--- frontend/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export function createEmptyList(qt:number) {
-  const fakeList = [];
-  for (let key = 1; key <= qt; key += 1) {
-    fakeList.push(key);
-  }
-  return fakeList;
-}
