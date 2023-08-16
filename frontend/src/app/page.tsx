0543bb9e4816aed157568a3ce65be12243faae75
davidrogger <davidrogger.a@gmail.com>
2023-08-16 08:38:22 -0300

    FEAT: change home page to client side

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,34 @@
+'use client';
+
 import LectureCard from '@/components/LectureCard';
 import { getAllLectures } from '@/services/api';
 import MainTitle from '@/components/MainTitle';
 import RefreshBtn from '@/components/RefreshBtn';
+import { useEffect, useState } from 'react';
+import { ILecture } from '@/types';
+import { useAuthContext } from '@/contexts/Auth';
 
-export default async function Home() {
-  const { lectures } = await getAllLectures();
+export default function Home() {
+  const { isAuthenticated, authStoredToken } = useAuthContext();
+  const [displayedLectures, setDisplayedLectures] = useState<ILecture[]>([]);
+
+  useEffect(() => {
+    async function loadLectures() {
+      const { lectures } = await getAllLectures();
+
+      if (lectures) setDisplayedLectures(lectures);
+    }
+
+    if (!isAuthenticated) authStoredToken();
+    if (!displayedLectures.length) loadLectures();
+  }, [displayedLectures, authStoredToken, isAuthenticated]);
 
   return (
     <div>
       <MainTitle title='Home' />
       <div className='flex flex-wrap content-start justify-center pt-8 h-[calc(100vh-112px)] bg-gray-100'>
-        {lectures
-          ? (lectures.map((lecture) => (
+        {displayedLectures
+          ? (displayedLectures.map((lecture) => (
           <LectureCard
             key={lecture.id}
             {...lecture}
