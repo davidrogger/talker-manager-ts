0415cba30edacd09aa7bb72fd7e14814b5b51b66
davidrogger <davidrogger.a@gmail.com>
2023-08-17 07:49:46 -0300

    TEST<home>: updated way to render the component

--- frontend/__tests__/pages/Home.test.tsx
+++ frontend/__tests__/pages/Home.test.tsx
@@ -1,22 +1,26 @@
 import Home from '@/app/page';
 import { api } from '@/services/api';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { mockLectures } from '../utils/_mockData';
 import RenderWithAuthProvider from '../utils/RenderWithAuthProvider';
 
 describe('Testing Home Page', () => {
+  beforeEach(jest.restoreAllMocks);
+
   it('Should render all elements from the Home page', async () => {
     jest.spyOn(api, 'get').mockResolvedValue({ data: { lectures: mockLectures } });
-    const jsx = await Home();
-    render(jsx);
+
+    RenderWithAuthProvider(<Home />);
+
     expect(screen.getByText('Home')).toBeInTheDocument();
-    expect(screen.getAllByText('Davíd Roggér')).toHaveLength(8);
+    expect(await screen.findAllByText('Davíd Roggér')).toHaveLength(8);
   });
 
   it('Should render a refresh button if something went wrong with the api request', async () => {
     jest.spyOn(api, 'get').mockRejectedValue({ error: 'fake-error' });
-    const jsx = await Home();
-    RenderWithAuthProvider(jsx);
+
+    RenderWithAuthProvider(<Home />);
+
     const refreshBtn = await screen.findByRole('button', { name: 'Refresh' });
     expect(refreshBtn).toBeInTheDocument();
     expect(refreshBtn).toBeVisible();
