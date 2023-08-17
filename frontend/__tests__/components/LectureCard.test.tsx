35d73dcc5d567c32839950462ed9e14aee1e27d7
davidrogger <davidrogger.a@gmail.com>
2023-08-17 14:48:23 -0300

    TEST: edit mode lecture card

--- frontend/__tests__/components/LectureCard.test.tsx
+++ frontend/__tests__/components/LectureCard.test.tsx
@@ -1,6 +1,7 @@
 import LectureCard from '@/components/LectureCard';
 import { render, screen } from '@testing-library/react';
 import { AuthContext, IAuthContext } from '@/contexts/Auth';
+import userEvent from '@testing-library/user-event';
 
 describe('Testing <LectureCard />', () => {
   const lectureCardTestingProps = {
@@ -8,6 +9,11 @@ describe('Testing <LectureCard />', () => {
     title: 'Testing Card',
     watchedAt: '17/08/2023',
   };
+  const RenderLectureCardAuthenticated = () => (
+    <AuthContext.Provider value={ { isAuthenticated: true } as IAuthContext }>
+      <LectureCard {...lectureCardTestingProps} />
+    </AuthContext.Provider>
+  );
 
   it('Should render the talkerName, title and watchedAt', () => {
     const expectedLectureCard = { ...lectureCardTestingProps };
@@ -20,16 +26,26 @@ describe('Testing <LectureCard />', () => {
   });
 
   it('Should render the edit and delete button when logged', async () => {
-    render(
-      <AuthContext.Provider value={ { isAuthenticated: true } as IAuthContext }>
-        <LectureCard {...lectureCardTestingProps} />
-      </AuthContext.Provider>,
-    );
+    render(<RenderLectureCardAuthenticated />);
 
     const editBtn = screen.getByTestId('test-edit-button');
     const deleteBtn = screen.getByTestId('test-delete-button');
 
     expect(editBtn).toBeVisible();
     expect(deleteBtn).toBeVisible();
+  });
+
+  it('Should change to editable mode element after clicked in the edit button', async () => {
+    const { talkerName, title, watchedAt } = lectureCardTestingProps;
+
+    render(<RenderLectureCardAuthenticated />);
+    const editBtn = screen.getByTestId('test-edit-button');
+    await userEvent.click(editBtn);
+
+    expect(screen.getByDisplayValue(talkerName)).toBeVisible();
+    expect(screen.getByDisplayValue(title)).toBeVisible();
+    expect(screen.getByText(watchedAt)).toBeVisible();
+    expect(screen.getByTestId('test-confirm-button')).toBeVisible();
+    expect(screen.getByTestId('test-cancel-button')).toBeVisible();
   });
 });
