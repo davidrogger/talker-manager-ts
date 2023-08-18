fe3a84a3b18bf0897abca4fa348be18d0a278921
davidrogger <davidrogger.a@gmail.com>
2023-08-18 12:00:40 -0300

    FIX<lecture update>: query update sintaxe

--- backend/src/models/lecture.model.ts
+++ backend/src/models/lecture.model.ts
@@ -7,12 +7,12 @@ import connection from '@models/connection.model';
 
 function normalizeLecture(rows: RowDataPacket[]):ILecture[] {
   const lectures: ILecture[] = rows.map(({
-    id, talkerName, title, watchedAt,
+    id, talkerName, title, watched_at,
   }) => ({
     id,
     talkerName,
     title,
-    watchedAt,
+    watchedAt: watched_at,
   }));
 
   return lectures;
@@ -20,7 +20,7 @@ function normalizeLecture(rows: RowDataPacket[]):ILecture[] {
 
 export async function getAllLectures() {
   const query = `
-    SELECT lecture.id, talker.name as talkerName, lecture.title, lecture.watchedAt
+    SELECT lecture.id, talker.name as talkerName, lecture.title, lecture.watched_at
     FROM lecture
     INNER JOIN talker
     ON talker.id = lecture.talker_id;`;
@@ -34,7 +34,7 @@ export async function getAllLectures() {
 export async function createLecture({
   id, talkerId, title, watchedAt,
 }:CreateLecture & { id:string }): Promise<void> {
-  const query = 'INSERT INTO lecture(id, talker_id, title, watchedAt) VALUES (?, ?, ?, ?)';
+  const query = 'INSERT INTO lecture(id, talker_id, title, watched_at) VALUES (?, ?, ?, ?)';
   await connection.execute(query, [id, talkerId, title, watchedAt]);
 }
 
@@ -46,6 +46,6 @@ export async function findLectureById(id:string) {
 }
 
 export async function updateLectureById(id:string, { talkerId, title, watchedAt }:UpdateLecture) {
-  const query = 'UPDATE lecture SET talker_id = ? title = ? watched_at = ? WHERE id = ?';
+  const query = 'UPDATE lecture SET talker_id = ?, title = ?, watched_at = ? WHERE id = ?';
   await connection.execute(query, [talkerId, title, watchedAt, id]);
 }
