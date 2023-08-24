8679b9ff5a1fb65791b8a4e34940305f0cdee5e7
davidrogger <davidrogger.a@gmail.com>
2023-08-24 16:40:12 -0300

    TEST<LectureCardPlus>: render an input title

--- frontend/__tests__/components/LectureCardPlus.test.tsx
+++ frontend/__tests__/components/LectureCardPlus.test.tsx
@@ -1,9 +1,10 @@
 import LectureCardPlus from '@/components/LectureSection/LectureCardPlus';
 import { AuthContext, IAuthContext } from '@/contexts/Auth';
 import { LectureProvider } from '@/contexts/Lectures';
+import { api } from '@/services/api';
 import { render, screen } from '@testing-library/react';
-import plusImg from '@/images/plus.svg';
-import { ClassAttributes, ImgHTMLAttributes } from 'react';
+import userEvent from '@testing-library/user-event';
+import { mockGetTalkersResponse } from '../utils/_mockData';
 
 describe('Testing Component <LectureCardPlus />', () => {
   beforeEach(jest.restoreAllMocks);
@@ -33,5 +34,21 @@ describe('Testing Component <LectureCardPlus />', () => {
     expect(buttonElement).toBeVisible();
     expect(imageElement).toBeVisible();
     expect(buttonElement).toContainElement(imageElement);
+  });
+
+  it('Should render inputtable title', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: { talkers: mockGetTalkersResponse } });
+    render(<LectureCardWithContext />);
+
+    const buttonElement = screen.getByRole<HTMLButtonElement>('button');
+    await userEvent.click(buttonElement);
+
+    const titleElement = await screen.findByTestId('test-title-input');
+    expect(titleElement).toBeVisible();
+    expect(titleElement).toHaveValue('');
+
+    const newInput = 'New Test title';
+    await userEvent.type(titleElement, newInput);
+    expect(titleElement).toHaveValue(newInput);
   });
 });
